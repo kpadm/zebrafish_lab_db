6aadc000c742916d6a57c627572ba489443f50e4
Karthik Padmanabhan <kpadm@users.noreply.github.com>
2021-06-14 08:00:23 -0700

    Update app.R

--- app.R
+++ app.R
@@ -15,32 +15,34 @@ library(shinyjs)
 library(uuid)
 library(dplyr)
 library(stringr)
+library(shinyalert)
+library(shinypop)
 
-zebrafish <- dbPool(RSQLite::SQLite(), dbname = "zfish.sqlite")
+zebrafish <- dbPool(RSQLite::SQLite(), dbname = "zfish3.sqlite")
 
 responses_df <- data.frame(Batch=integer(),
-                   Name=character(),
-                   Birthday=as.Date(character()),
-                   Death=as.Date(character()),
-                   Status=character(),
-                   User=character(),
-                   Strain=character(),
-                   Generation=character(),
-                   Adults=integer(),
-                   Nursery=integer(),
-                   Mother=integer(), 
-                   Father=integer(),
-                   Notes=character())
+                           Name=character(),
+                           Birthday=as.Date(character()),
+                           Death=as.Date(character()),
+                           Status=character(),
+                           User=character(),
+                           Strain=character(),
+                           Generation=character(),
+                           Adults=integer(),
+                           Nursery=integer(),
+                           Mother=integer(), 
+                           Father=integer(),
+                           Notes=character())
 
 dbWriteTable(zebrafish, "responses_df", responses_df, overwrite = FALSE, append = TRUE)
 
 jsCode <- 'shinyjs.winprint = function (){
-              var divToPrint=document.getElementById("print-form");
-              newWin= window.open("");
-              newWin.document.write(divToPrint.outerHTML);
-              newWin.print();
-              newWin.close();
-          }'
+var divToPrint=document.getElementById("print-form");
+newWin= window.open("");
+newWin.document.write(divToPrint.outerHTML);
+newWin.print();
+newWin.close();
+}'
 
 
 
@@ -57,20 +59,22 @@ appCSS <- ".mandatory_star { color: red; }"
 # ui
 ui <- fluidPage(
   shinyjs::useShinyjs(),
+  use_notie(),
+  useShinyalert(),
   shinyjs::inlineCSS(appCSS),
   extendShinyjs(text = jsCode, functions = c("winprint")),
   titlePanel("Baraban Lab Zebrafish Manager"),
   fluidRow(
     actionButton("add_button", "Add", icon("fish")),
     actionButton("edit_button", "Edit", icon("edit")),
-    actionButton("copy_button", "Copy", icon("copy")),
+    #actionButton("copy_button", "Copy", icon("copy")),
     actionButton("delete_button", "Delete", icon("trash-alt")),
     actionButton("print_button", "Print", icon("print")),
     downloadButton("zebrafish_csv", "Download CSV", class="butt")
   ),
   br(),
   fluidRow(width="100%",
-           dataTableOutput("responses_table", width = "100%")
+           DT::DTOutput("responses_table", width = "100%")
   ), 
   fluidRow(width="25%", 
            dataTableOutput("total_table", width="25%"))
@@ -85,14 +89,13 @@ server <- function(input, output, session) {
     #make reactive to
     input$submit
     input$submit_edit
-    input$copy_button
+    #input$copy_button
     input$delete_button
     input$print_button
-
     dbReadTable(zebrafish, "responses_df")
     
   })  
-
+  
   
   #List of mandatory fields for submission
   fieldsMandatory <- c("Name", "Status", "User", "Strain", "Generation")
@@ -130,7 +133,7 @@ server <- function(input, output, session) {
                 selectInput("Strain", labelMandatory("Strain"), 
                             choices=c("TL", "AB", "TLAB")),  
                 selectInput("Generation", labelMandatory("Generation"),
-                              choices=c("NA","F0","F1", "F2", "F3", "F4", "F5"), selected = "NA"),
+                            choices=c("NA","F0","F1", "F2", "F3", "F4", "F5"), selected = "NA"),
                 numericInput("Adults", "Adults", 0),
                 numericInput("Nursery", "Nursery", 0),
                 selectInput("Mother", "Mother", choices = c(" ", Batch_Select)),
@@ -150,26 +153,26 @@ server <- function(input, output, session) {
   
   #save form data into data_frame format
   SQL_df <- 
-  formData <- reactive({
-    
-    formData <- data.frame(Batch = ifelse((length(dbReadTable(zebrafish, "responses_df")[,c("Batch")]) == 0), 1, 
-                                          (max(dbReadTable(zebrafish, "responses_df")[,c("Batch")]) + 1)),
-                           Name = input$Name,
-                           User = input$User,
-                           Strain = input$Strain,
-                           Status = input$Status,
-                           Death = format(as.Date(input$Death)),
-                           Generation = input$Generation,
-                           Adults = input$Adults,
-                           Nursery = input$Nursery,
-                           Birthday = format(as.Date(input$Birthday)),
-                           Mother = input$Mother,
-                           Father = input$Father,
-                           Notes = input$Notes,
-                           stringsAsFactors = FALSE)
-    return(formData)
-    
-  })
+    formData <- reactive({
+      
+      formData <- data.frame(Batch = ifelse((length(dbReadTable(zebrafish, "responses_df")[,c("Batch")]) == 0), 1, 
+                                            (max(dbReadTable(zebrafish, "responses_df")[,c("Batch")]) + 1)),
+                             Name = input$Name,
+                             User = input$User,
+                             Strain = input$Strain,
+                             Status = input$Status,
+                             Death = format(as.Date(input$Death)),
+                             Generation = input$Generation,
+                             Adults = input$Adults,
+                             Nursery = input$Nursery,
+                             Birthday = format(as.Date(input$Birthday)),
+                             Mother = input$Mother,
+                             Father = input$Father,
+                             Notes = input$Notes,
+                             stringsAsFactors = FALSE)
+      return(formData)
+      
+    })
   
   appendData <- function(data){
     quary <- sqlAppendTable(zebrafish, "responses_df", data, row.names = FALSE)
@@ -204,15 +207,15 @@ server <- function(input, output, session) {
         obj = str_split(name_selection, ";")
         modalDialog(
           div(id=("print-form"),
-            renderUI(HTML(do.call(paste, c(obj, collapse = "<br/>")))),
-            renderText(paste("Batch: ",batch_selection), quoted=FALSE),
-            renderText(paste("D.O.B: ", birthday_selection), quoted=FALSE),
-            renderText(user_selection, quoted=FALSE)), easyClose=TRUE, 
-            size = "s",
-            footer = tagList(
-              modalButton("Dismiss"),
-              actionButton("print_window", "Print")
-            )
+              renderUI(HTML(do.call(paste, c(obj, collapse = "<br/>")))),
+              renderText(paste("Batch: ",batch_selection), quoted=FALSE),
+              renderText(paste("D.O.B: ", birthday_selection), quoted=FALSE),
+              renderText(user_selection, quoted=FALSE)), easyClose=TRUE, 
+          size = "s",
+          footer = tagList(
+            modalButton("Dismiss"),
+            actionButton("print_window", "Print")
+          )
         )
       } else if (length(input$responses_table_rows_selected)>1){
         modalDialog(
@@ -225,7 +228,7 @@ server <- function(input, output, session) {
           paste("Please select row(s)." ),easyClose = TRUE
         )
       }
-      )
+    )
     
     
   })
@@ -245,6 +248,7 @@ server <- function(input, output, session) {
   deleteData <- reactive({
     
     SQL_df <- dbReadTable(zebrafish, "responses_df")
+    print(input$responses_table_rows_selected)
     row_selection <- SQL_df[input$responses_table_rows_selected, "Batch"]
     
     quary <- lapply(row_selection, function(nr){
@@ -253,54 +257,73 @@ server <- function(input, output, session) {
     })
   })
   
-  observeEvent(input$delete_button, priority = 20,{
-    
-    if(length(input$responses_table_rows_selected)>=1 ){
-      deleteData()
-    }
+
+  observeEvent(input$delete_button,{
     
     showModal(
-      
-      if(length(input$responses_table_rows_selected) < 1 ){
-        modalDialog(
-          title = "Warning",
-          paste("Please select row(s)." ),easyClose = TRUE
-        )
+     if(length(input$responses_table_rows_selected)>=1 ){
+       modalDialog(
+         title = "Confirm deletion",
+         paste("Do you want to delete?"),
+         footer = tagList(
+           modalButton("No"),
+           actionButton("confirm_delete", "Yes")
+         )
+       )
+     }else if (length(input$responses_table_rows_selected) < 1 ){
+          modalDialog(
+            title = "Warning",
+            paste("Please select row(s)." ),easyClose = TRUE
+          )
       })
   })
+  
+  observeEvent(input$confirm_delete, {
+    #vals_trich$Data=vals_trich$Data[-input$Main_table_trich_rows_selected]
+    SQL_df <- dbReadTable(zebrafish, "responses_df")
+    print(input$responses_table_rows_selected)
+    row_selection <- SQL_df[input$responses_table_rows_selected, "Batch"]
+    
+    quary <- lapply(row_selection, function(nr){
+      
+      dbExecute(zebrafish, sprintf('DELETE FROM "responses_df" WHERE "Batch" == ("%s")', nr))
+    })
+    removeModal()
+  })
+
   
   unique_id <- function(data){
     replicate(nrow(data),round(as.numeric(Sys.time())))
   }
   
-  copyData <- reactive({
-    
-    SQL_df <- dbReadTable(zebrafish, "responses_df")
-    row_selection <- SQL_df[input$responses_table_rows_selected, "Batch"] 
-    SQL_df <- SQL_df %>% filter(Batch %in% row_selection)
-    SQL_df$Batch <- unique_id(SQL_df)
-    
-    quary <- sqlAppendTable(zebrafish, "responses_df", SQL_df, row.names = FALSE)
-    dbExecute(zebrafish, quary)
-    
-  })
-  
-  observeEvent(input$copy_button, priority = 20,{
-    
-    if(length(input$responses_table_rows_selected)>=1 ){
-      copyData()
-    }
-    
-    showModal(
-      
-      if(length(input$responses_table_rows_selected) < 1 ){
-        modalDialog(
-          title = "Warning",
-          paste("Please select row(s)." ),easyClose = TRUE
-        )
-      })
-    
-  })
+  # copyData <- reactive({
+  #   
+  #   SQL_df <- dbReadTable(zebrafish, "responses_df")
+  #   row_selection <- SQL_df[input$responses_table_rows_selected, "Batch"] 
+  #   SQL_df <- SQL_df %>% filter(Batch %in% row_selection)
+  #   SQL_df$Batch <- unique_id(SQL_df)
+  #   
+  #   quary <- sqlAppendTable(zebrafish, "responses_df", SQL_df, row.names = FALSE)
+  #   dbExecute(zebrafish, quary)
+  #   
+  # })
+  # 
+  # observeEvent(input$copy_button, priority = 20,{
+  #   
+  #   if(length(input$responses_table_rows_selected)>=1 ){
+  #     copyData()
+  #   }
+  #   
+  #   showModal(
+  #     
+  #     if(length(input$responses_table_rows_selected) < 1 ){
+  #       modalDialog(
+  #         title = "Warning",
+  #         paste("Please select row(s)." ),easyClose = TRUE
+  #       )
+  #     })
+  #   
+  # })
   
   observeEvent(input$edit_button, priority = 20,{
     
@@ -349,7 +372,7 @@ server <- function(input, output, session) {
     SQL_df <- dbReadTable(zebrafish, "responses_df")
     row_selection <- SQL_df[input$responses_table_row_last_clicked, "Batch"] 
     dbExecute(zebrafish, sprintf('UPDATE "responses_df" SET "Name" = ?, "Birthday" = ?, "User" = ?, "Status" = ?, "Death" = ?,
-                            "Generation" = ?, "Strain" = ?, "Adults" = ?, "Nursery" = ?, "Mother" = ?, "Father" = ?, "Notes" = ? 
+                                 "Generation" = ?, "Strain" = ?, "Adults" = ?, "Nursery" = ?, "Mother" = ?, "Father" = ?, "Notes" = ? 
                                  WHERE "Batch" = ("%s")', row_selection), 
               param = list(input$Name,
                            format(as.Date(input$Birthday)),
@@ -369,11 +392,11 @@ server <- function(input, output, session) {
   
   output$responses_table <- DT::renderDataTable({
     
-    table <- responses_df()
-    table <- table[,c(1:12)]
-    names(table) <- c("Batch", "Name", "Birthday","Death", "Status", "User", "Strain", "Generation",
+    res.table <- responses_df()
+    res.table <- res.table[,c(1:12)]
+    names(res.table) <- c("Batch", "Name", "Birthday","Death", "Status", "User", "Strain", "Generation",
                       "Adults", "Nursery","Mother", "Father")
-    table <- datatable(table, 
+    res.table <- datatable(res.table, 
                        rownames = FALSE,
                        options = list(searching = TRUE, lengthChange = FALSE, 
                                       pageLength = 20, order = list(0, 'desc'),
@@ -384,6 +407,8 @@ server <- function(input, output, session) {
     )
     
   })
+  
+  #outputOptions(output, "responses_table", suspendWhenHidden = FALSE)
   
   output$total_table <- DT::renderDataTable({
     
@@ -396,6 +421,7 @@ server <- function(input, output, session) {
                        options = list(searching = FALSE, lengthChange = FALSE, dom = 't'))
     
   })
+  
   
 }
 
